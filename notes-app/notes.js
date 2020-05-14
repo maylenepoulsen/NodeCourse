72bb29798474588d613d1766561aeace6021d759
github username <github email address>
2020-05-14 17:10:51 -0500

    finish add notes node app

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,15 +1,11 @@
 const fs = require("fs");
 const chalk = require("chalk");
 
-const getNotes = () => {
-  return "Your notes...";
-};
-
 const addNote = (title, body) => {
   const notes = loadNotes();
-  const duplicateNotes = notes.filter((note) => note.title === title);
+  const duplicateNote = notes.find((note) => note.title === title);
 
-  if (duplicateNotes.length === 0) {
+  if (!duplicateNote) {
     notes.push({
       title,
       body,
@@ -40,6 +36,18 @@ const listNotes = () => {
   notes.forEach((note) => console.log(chalk.hex('#42eff5')(note.title)))
 }
 
+const readNote = (title) => {
+  const notes = loadNotes()
+  const note = notes.find((note) => note.title === title)
+
+  if(note) {
+    console.log(chalk.blue(note.title))
+    console.log(note.body)
+  } else {
+    console.log(chalk.red('Note was not found'))
+  }
+}
+
 const saveNotes = (notes) => {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync("notes.json", dataJSON);
@@ -56,8 +64,8 @@ const loadNotes = () => {
 };
 
 module.exports = {
-  getNotes,
   addNote,
   removeNote,
-  listNotes
+  listNotes,
+  readNote
 };
