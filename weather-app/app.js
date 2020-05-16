8f6be324459ccc7a8b62f6279f6e2c5cc90112bb
maylenepoulsen <mayleneski@hotmail.com>
2020-05-16 13:37:02 -0500

    uncomment

--- weather-app/app.js
+++ weather-app/app.js
@@ -16,8 +16,8 @@ request({ url: url, json: true }, (error, response) => {
   }
 });
 
-// request({ url: mapBoxUrl, json: true }, (error, response) => {
-//     const latitude = response.body.features[0].center[1];
-//     const longitude = response.body.features[0].center[0];
-//     console.log(latitude, longitude)
-// })
+request({ url: mapBoxUrl, json: true }, (error, response) => {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log(latitude, longitude)
+})
