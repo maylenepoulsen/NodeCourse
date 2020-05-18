9b3ec8a427b554ca37360a546a9f5549308e07d3
maylenepoulsen <mayleneski@hotmail.com>
2020-05-17 19:17:44 -0500

    destructuring

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,19 +3,19 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
   const url = 'http://api.weatherstack.com/current?access_key=9a8be277d5082cd17452f38365ab9c8f&query=' + latitude + ',' + longitude + '&units=f';
 
-  request( {url: url, json: true}, (error, response) => {
+  request( { url, json: true}, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service.', undefined)    
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback('Unable to find location', undefined)
     } else {
-        const data = response.body.current;
-        let weatherCast = response.body.current.weather_descriptions[0];
+        const { temperature, feelslike } = body.current;
+        let weatherCast = body.current.weather_descriptions[0];
 
         if (weatherCast === undefined) {
             weatherCast = 'Unable to find weather cast'
         }
-        callback(undefined, `${weatherCast}. It is currently ${data.temperature} degrees outside. It feels like ${data.feelslike} degrees outside.`)
+        callback(undefined, `${weatherCast}. It is currently ${temperature} degrees outside. It feels like ${feelslike} degrees outside.`)
     }
   })
 };
