6c11bdc0a6e83f04b0bb2fe5add9ff254c69228a
maylenepoulsen <mayleneski@hotmail.com>
2020-05-19 16:45:19 -0500

    using req.query

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -40,11 +40,30 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address'
+    })
+  }
+
   res.send({
       forecast: 'Hot and Sunny, about 90 degrees',
-      location: 'Cedar Park, TX'
+      location: 'Cedar Park, TX',
+      address: req.query.address
   });   
 });
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term'
+    })
+  }
+  console.log(req.query.search)
+  res.send({
+    products: []
+  })
+})
 
 app.get('/help/*', (req, res) => {
   res.render('404', {
