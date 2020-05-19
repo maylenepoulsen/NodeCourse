be40227692c92a0e61effaf149251d5267be0bc4
maylenepoulsen <mayleneski@hotmail.com>
2020-05-19 11:37:58 -0500

    rendering dynamic hbs files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,9 +2,31 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-
 const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Maylene Poulsen'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Maylene Poulsen'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help Page',
+    message: 'Refer back to the Node Course videos if you are stuck on some of the content.'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
